c716f0f35063b0b1ccd96acdd008e34386bbc471
sosout <pss77585211>
2018-02-03 23:17:34 +0800

    调整目录

--- build/build-style.js
+++ build/build-style.js
@@ -8,18 +8,18 @@ const del = require('del');
 
 // 清空文件
 gulp.task('clean', function () {
-  del.sync(['../dist/**', '../src/components/styles/**'], { force: true });
+  del.sync(['../dist/**'], { force: true });
 });
 
 // 编译less
 gulp.task('compileCss', function () {
-  gulp.src('../src/styles/*.less')
+  gulp.src('../src/components/styles/*.less')
     .pipe(less())
     .pipe(autoprefixer({
       browsers: ['last 2 versions', 'ie > 8'],
       cascade: false
     }))
-    .pipe(gulpif('!index.css', gulp.dest('../src/components/styles'))) // 组件样式
+    // .pipe(gulpif('!index.css', gulp.dest('../src/components/styles'))) // 组件样式
     .pipe(gulpif('index.css', rename('very-vue.css'))) // 未压缩的核心css文件夹
     .pipe(gulpif('very-vue.css', gulp.dest('../dist')))
     .pipe(gulpif('very-vue.css', rename('very-vue.min.css'))) // 重命名核心css文件夹
@@ -30,7 +30,7 @@ gulp.task('compileCss', function () {
 // 拷贝字体文件
 gulp.task('fonts', function () {
   gulp.src('../src/styles/iconfont/fonts/*.*')
-    .pipe(gulp.dest('../dist/styles/fonts'));
+    .pipe(gulp.dest('../dist/fonts'));
 });
 
 gulp.task('default', ['clean', 'compileCss', 'fonts']);
